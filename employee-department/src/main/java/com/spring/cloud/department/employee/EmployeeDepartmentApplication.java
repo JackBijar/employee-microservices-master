6746a33048dd892a5eff99cdb69659b16e29388c
rajibgarai <rajibgarai@gmail.com>
2017-12-13 21:20:26 +0530

    Third time commit add new microservice "Employee-Department"

---
+++ employee-department/src/main/java/com/spring/cloud/department/employee/EmployeeDepartmentApplication.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.department.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDepartmentApplication.class, args);
+	}
+}
